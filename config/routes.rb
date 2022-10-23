8f5e03520b75f87c69742e4a7074b70820563f3d
Allan <kipropsallan@gmail.com>
2022-10-24 00:07:21 +0300

    Updated recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: [:index, :create, :show, :update, :destroy]
+  resources :recipes, only: [:index, :create, :show, :update, :destroy]
+
 end
