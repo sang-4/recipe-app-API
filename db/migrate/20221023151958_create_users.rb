d172587f5693d8d011a70ab0ceee2381266ab309
Allan <kipropsallan@gmail.com>
2022-10-23 18:53:31 +0300

    Updated models

--- db/migrate/20221023151958_create_users.rb
+++ db/migrate/20221023151958_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :username
       t.string :password_digest
 
       t.timestamps
