d4f50724e07316892fa005ff744b2c2d5691c278
Allan <kipropsallan@gmail.com>
2022-10-23 19:15:53 +0300

    Updated HTTP error codes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     def index
         @users = User.all
         render json: @users, status: 200
@@ -11,7 +14,6 @@ class UsersController < ApplicationController
 
     def create
         puts JSON.pretty_generate(user_params)
-
         new_user = User.create!(user_params)
         render json: new_user, status: :created
     end
@@ -22,10 +24,19 @@ class UsersController < ApplicationController
         render json: @user, status: 200
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {message: "User deleted"}, status: 200
+    end
+
     private
 
     def user_params
         params.require(:user).permit(:id, :first_name, :last_name, :username, :email, :password_digest)
+    end
 
+    def record_not_found
+        render json: {message: "User not found"}, status: 404
     end
 end
