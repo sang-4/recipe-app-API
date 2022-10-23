36689ab7f4d45c9dc7324f617ca6db2894904527
Allan <kipropsallan@gmail.com>
2022-10-23 18:53:13 +0300

    Updated models and tested out all endpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_many :recipes
+
+    has_secure_password
+
+    validates :first_name, :last_name, :email, :username, presence: true
+    # validates :username, presence: true, uniqueness: true
+    # validates :email, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
 end
