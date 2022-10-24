72853d883d3e98fefbec66262536869e44c73498
Moringa School <maryanneanyango98@gmail.com>
2022-10-24 17:20:17 +0300

    deleted users resource

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-    has_many :recipes
-    has_many :comments
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :email, uniqueness: true 
-end
