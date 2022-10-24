03120b749c36c9401759eebb7def15629adc4bc1
Allan Kiprop <81680143+sang-4@users.noreply.github.com>
2022-10-24 07:41:47 -0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
