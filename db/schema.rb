8badd51eae7d9e124e8cba3d43f6e5f9c42aac3a
Allan <kipropsallan@gmail.com>
2022-10-24 17:46:11 +0300

    Updated migration tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_203951) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.integer "num_of_pple_served"
+    t.string "country"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
