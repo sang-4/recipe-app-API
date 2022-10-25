a0d06ec0408c5f1555f3fdc213502bc264c0a8fe
Allan <kipropsallan@gmail.com>
2022-10-25 17:24:11 +0300

    Updater sessions functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :update, :destroy]
   resources :recipes, only: [:index, :create, :show, :update, :destroy]
 
+  post :login, to: "sessions#login"
+
 end
