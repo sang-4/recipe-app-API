99a40694678e1fb6deef7391323e64733cf52787
Allan <kipropsallan@gmail.com>
2022-10-26 20:48:49 +0300

    modified models

--- db/migrate/20221023203951_create_recipes.rb
+++ db/migrate/20221023203951_create_recipes.rb
@@ -1,11 +1,14 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string :foodname
       t.string :ingredients
-      t.integer :num_of_pple_served
+      t.integer :servings
       t.string :country
       t.integer :rating
+      t.string :image
+      t.string :description
+      t.string :instructions
 
       t.timestamps
     end
