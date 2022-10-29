797b87bf9a2a99dda175ea18f1090de2bbe94fa0
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-10-29 23:27:05 +0300

    modifies user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :recipes
     has_many :comments, dependent: :destroy
 
     has_secure_password
 
-    validates :firstname, :lastname, :username, :email, presence: true
+    validates :firstname, :lastname, :username,presence: true
     validates :password, length: { minimum: 6 }
     validates :email, uniqueness: true
 
