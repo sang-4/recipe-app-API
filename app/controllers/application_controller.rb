7a7e73ae6e2973c2931dd9484077e58ac0c4740c
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-10-29 23:23:35 +0300

    modifies app controllers with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,30 @@
 class ApplicationController < ActionController::API
 include ActionController::Cookies
 
-rescue_from ActiveRecord::RecordNotFound, with: :render_unprocessable_entity_response
+# rescue_from ActiveRecord::RecordNotFound, with: :render_unprocessable_entity_response
 
-before_action :authorize
+# before_action :authorize
 
-def current_user
-    User.find_by(id: session[:current_user])
-end
+# def current_user
+#     User.find_by(id: session[:current_user])
+# end
 
-private
+# private
 
-    def authorize
-        @current_user = User.find_by(id: session[:user_id])
-        render json: {message: "You are not authorized to view this page"}, status: 401 unless @current_user
+#     def authorize
+#         @current_user = User.find_by(id: session[:user_id])
+#         render json: {message: "You are not authorized to view this page"}, status: 401 unless @current_user
+#     end
+
+#     def render_unprocessable_entity_response(exception)
+#         render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+#     end
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
     end
-
-    def render_unprocessable_entity_response(exception)
-        render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
-    end
-
+  end
 end
