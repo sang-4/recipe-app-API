a6d921da19014ad1f4713b7047705b87b03f64cc
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-10-29 23:33:04 +0300

    modifies schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_203951) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_191138) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.string "content"
@@ -33,11 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_203951) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
     t.string "email"
-    t.string "username"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
