54eb917ff2bbbfc1f915fe67624dc32805aa9209
Allan <kipropsallan@gmail.com>
2022-10-31 21:20:01 +0300

    finalized on sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,11 @@
 class UsersController < ApplicationController
-
     skip_before_action :authorize, only: :create
-    
-    before_action :authenticate_request!, only: [:index, :show, :update, :destroy
-rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-# rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
-
-def show
-    render json: { success: true, username: @user }
-end 
-    
-    def new
-        user = User.new(cookies[:user_id])
-    end
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
         users = User.all
-        render json: users, status: 200
+        render json: users
     end
 
     def show
@@ -27,29 +15,25 @@ end
 
     def create
         user = User.create!(user_params)
-        session[:user_id] = user.id
+        session[:current_user] = user.id
         render json: user, status: :created
     end
 
+
     def update
         user = User.find(params[:id])
-        user.update(user_params)
-        render json: user, status: 200
+        user.update!(user_params)
+        render json: user, status: :accepted
     end
 
     def destroy
         user = User.find(params[:id])
         user.destroy
-        render json: {message: "User deleted"}, status: 200
+        render json: {message: "User deleted"}, status: :ok
     end
 
     private
-
     def user_params
-        params.permit(:firstname, :lastname, :username, :email, :password, :password_confirmation)
-    end
-
-    def record_not_found
-        render json: {message: "User not found"}, status: 404
+        params.permit(:firstname, :lastname, :email, :username, :password, :password_confirmation)
     end
 end
