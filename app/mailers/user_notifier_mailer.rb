f033660ecfad912ce249b863970db5d4e78520b6
Allan <kipropsallan@gmail.com>
2022-11-01 11:38:20 +0300

    Mailer configurations

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,11 @@
+class UserNotifierMailer < ApplicationMailer
+    default :from => 'ksakiprs@gmail.com'
+  
+    # send a signup email to the user, pass in the user object that   contains the user's email address
+    def send_signup_email(user)
+      @user = user
+      mail( :to => @user.email,
+      :subject => 'Thanks for signing up for an account with us' )
+    end
+  end
+  
