6558a5290ed679b48e1705c226aa9549177d0389
Allan <kipropsallan@gmail.com>
2022-11-01 12:58:22 +0300

    updated API authentication

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,32 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# ActionMailer::Base.smtp_settings
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  :password => '<SENDGRID_API_KEY>', # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'gmail.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+# using SendGrid's Ruby Library
+# https://github.com/sendgrid/sendgrid-ruby
+require 'sendgrid-ruby'
+include SendGrid
+
+from = Email.new(email: 'ksakiprs@gmail.com')
+to = Email.new(email: 'kipropsallan@gmail.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+
