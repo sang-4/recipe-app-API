e8146fcb4ee838c09ab71b4e4a463367bcd62988
Allan <kipropsallan@gmail.com>
2022-11-03 19:06:49 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
     end
 
     private
+    
     def user_params
         params.permit(:firstname, :lastname, :email, :username, :password, :password_confirmation)
     end
